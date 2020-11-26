7655f91a898d6e676511435b75fd6a8cbc5aa0b4
Faycel CHAOUA <faycelchaoua@gmail.com>
2020-11-26 11:28:52 +0100

    Gradle Update

--- app/src/main/java/com/isetkelibia/customlistview/MainActivity.java
+++ app/src/main/java/com/isetkelibia/customlistview/MainActivity.java
@@ -2,11 +2,8 @@ package com.isetkelibia.customlistview;
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -23,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Récupération de la "ListView" créée dans le fichier activity_main.xml
         maListViewPerso = findViewById(R.id.listviewperso);
 
@@ -35,16 +31,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Création d'une "HashMap" pour insérer les informations du premier item de notre "ListView"
         map = new HashMap<>();
-        // On insère un élément "titre" que l'on récupérera dans le "TextView titre" créé dans le fichier affichage_item.xml
+        // On insère un élément "titre" que l'on récupérera dans le "TextView titre" créé dans
+        // le fichier affichage_item.xml
         map.put("titre", "Word");
-        // On insère un élément "description" que l'on récupérera dans le "TextView description" créé dans le fichier affichage_item.xml
+        // On insère un élément "description" que l'on récupérera dans le "TextView description"
+        // créé dans le fichier affichage_item.xml
         map.put("description", "Editeur de texte");
-        // On insère la "référence" à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans le "ImageView" créé dans le fichier affichage_item.xml
+        // On insère la "référence" à l'image (convertit en String car normalement c'est un int)
+        // que l'on récupérera dans le "ImageView" créé dans le fichier affichage_item.xml
         map.put("img", String.valueOf(R.drawable.word));
         // Enfin on ajoute cette "HashMap" dans la "ArrayList"
         listItem.add(map);
 
-        // On refait la manip plusieurs fois avec des données différentes pour former les items de notre "ListView"
+        // On refait la manip plusieurs fois avec des données différentes pour former les items
+        // de notre "ListView"
 
         map = new HashMap<>();
         map.put("titre", "Excel");
@@ -64,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
         map.put("img", String.valueOf(R.drawable.outlook));
         listItem.add(map);
 
-        // Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre liste (listItem) dans la vue affichage_item
+        // Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre
+        // liste (listItem) dans la vue affichage_item
         SimpleAdapter adapter = new SimpleAdapter(this.getBaseContext(),
                 listItem,
                 R.layout.activity_list_item,
@@ -73,36 +74,32 @@ public class MainActivity extends AppCompatActivity {
 
         // On attribue à notre "ListView" l'adapter que l'on vient de créer
         maListViewPerso.setAdapter(adapter);
-        maListViewPerso.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Méthode 1
-                HashMap map = (HashMap) maListViewPerso.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, (String) map.get("titre"),
-                        Toast.LENGTH_SHORT).show();
-                // Méthode 2
-                /*String selectedItem = ((TextView) view.findViewById(R.id.titre)).getText().toString();
-                Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();*/
-            }
+
+        maListViewPerso.setOnItemClickListener((parent, view, position, id) -> {
+            // Méthode 1
+            HashMap map1 = (HashMap) maListViewPerso.getItemAtPosition(position);
+            String titre = (String) map1.get("titre");
+            Toast.makeText(MainActivity.this, titre, Toast.LENGTH_SHORT).show();
+            // Méthode 2
+            /*String selectedItem = ((TextView) view.findViewById(R.id.titre)).getText().toString();
+            Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();*/
         });
 
-        maListViewPerso.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                // On récupère la "HashMap" contenant les infos de notre item (titre, description, img)
-                HashMap map = (HashMap) maListViewPerso.getItemAtPosition(position);
-                // On crée une boite de dialogue
-                AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
-                // On attribue un titre à notre boite de dialogue
-                adb.setTitle("Sélection Item");
-                // On insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
-                adb.setMessage("Votre choix : " + map.get("titre"));
-                // On indique que l'on veut le bouton "ok" à notre boite de dialogue
-                adb.setPositiveButton("Ok", null);
-                // On affiche la boite de dialogue
-                adb.show();
-                return true;
-            }
+        maListViewPerso.setOnItemLongClickListener((parent, view, position, id) -> {
+            // On récupère la "HashMap" contenant les infos de notre item (titre, description, img)
+            HashMap map12 = (HashMap) maListViewPerso.getItemAtPosition(position);
+            // On crée une boite de dialogue
+            AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+            // On attribue un titre à notre boite de dialogue
+            adb.setTitle("Sélection Item");
+            // On insère un message à notre boite de dialogue, et ici on affiche le titre
+            // de l'item cliqué
+            adb.setMessage("Votre choix : " + map12.get("titre"));
+            // On indique que l'on veut le bouton "ok" à notre boite de dialogue
+            adb.setPositiveButton("Ok", null);
+            // On affiche la boite de dialogue
+            adb.show();
+            return true;
         });
     }
 }
